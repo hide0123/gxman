9787c703769a14d394c4e494361547830bf0666e
HIDE810 <48782538+HIDE810@users.noreply.github.com>
2021-11-15 01:52:34 +0900

    show version

--- cmd/root.go
+++ cmd/root.go
@@ -1,6 +1,9 @@
 package cmd
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/spf13/cobra"
 )
 
@@ -10,6 +13,19 @@ var rootCmd = &cobra.Command{
 	Long: `gxman is a CLI tool for 3gxtool.
 This tool is used to install the latest one
 or to check the type of the installed one.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		version, err := cmd.Flags().GetBool("version")
+		if err != nil {
+			fmt.Println("error", err)
+			os.Exit(1)
+		}
+
+		if version {
+			fmt.Println("gxman version 1.0.0")
+		} else {
+			cmd.Help()
+		}
+	},
 }
 
 func Execute() {
@@ -18,4 +34,5 @@ func Execute() {
 
 func init() {
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.Flags().BoolP("version", "v", false, "print the version")
 }
